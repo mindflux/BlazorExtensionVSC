dce43bfdbab28fad4014457ae950fbd0e7beee75
mindflux <mindflux98@gmail.com>
2022-10-10 20:06:46 -0500

    refactor

--- src/extension.ts
+++ src/extension.ts
@@ -2,62 +2,60 @@ import * as vscode from 'vscode';
 import { getTemplatedComponentCodebehind, getTemplatedComponentRazor } from './templateFiles';
 const fs = require("fs");
 const path = require("path");
-const currentOS = process.platform;
+const isWindows: boolean = process.platform === 'win32' ? true : false;
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(
-		vscode.commands.registerCommand('blazorextension.newBlazorComponent', async (fileUri) => {
-			let componentNameNullable = await vscode.window.showInputBox({ "placeHolder": "Enter component name" });
+    context.subscriptions.push(
+        vscode.commands.registerCommand('blazorextension.newBlazorFComponent', async (fileUri) => {
+            let componentNameNullable = await vscode.window.showInputBox({ "placeHolder": "Enter component name" });
 
-			if (componentNameNullable === undefined) {
-				vscode.window.showErrorMessage("Component was undefined");
-				return;
-			}
+            if (componentNameNullable === undefined) {
+                vscode.window.showErrorMessage("Component was undefined");
+                return;
+            }
 
-			let componentName: string = componentNameNullable;
-			let filePath: string = fileUri.fsPath;
+            let componentName: string = componentNameNullable;
+            let filePath: string = fileUri.fsPath;
 
-			var startChar = currentOS !== 'win32' ? 1 : filePath.indexOf(":") + 2; //strip off first forward slash for linux or drive letter : \ for windows
+            let startChar: number = !isWindows ? 1 : filePath.indexOf(":") + 2; //strip off first forward slash for linux or drive letter : \ for windows
 
-			filePath = filePath.substring(startChar);
+            filePath = filePath.substring(startChar);
 
-			var delim = currentOS !== 'win32' ? '/' : '\\'; //linux v windows
-			var pathArray = filePath.split(delim);
+            let delim: string = !isWindows ? '/' : '\\'; //linux v windows
+            let pathArray: string[] = filePath.split(delim);
 
-			//loop through array to uppercase each char of first segment 
-			//old method wasn't working properly
-			for (let i = 0; i < pathArray.length; i++) {
-				pathArray[i] = pathArray[i].charAt(0).toUpperCase() + pathArray[i].slice(1);
-			}
+            //loop through array to uppercase each char of first segment 
+            //old method wasn't working properly
+            pathArray.forEach((e, i, a) => { a[i] = e.charAt(0).toUpperCase() + e.slice(1); });
 
-			filePath = pathArray.join('.');
+            filePath = pathArray.join('.');
 
-			let namespaceNullable = await vscode.window.showInputBox({
-				"placeHolder": "Enter namespace or clear field to stop creation of codebehind",
-				"value": filePath
-			});
+            let namespaceNullable = await vscode.window.showInputBox({
+                "placeHolder": "Enter namespace or clear field to stop creation of codebehind",
+                "value": filePath
+            });
 
-			let folderPath = fileUri.fsPath;
+            let folderPath = fileUri.fsPath;
 
-			await createFile(folderPath, `${componentName}.razor`, getTemplatedComponentRazor(componentName));
+            await createFile(folderPath, `${componentName}.razor`, getTemplatedComponentRazor(componentName));
 
-			//only create codebehind if namespace is given
-			if (namespaceNullable !== undefined && namespaceNullable.length > 0) {
-				let namespace: string = namespaceNullable;
-				await createFile(folderPath, `${componentName}.razor.cs`, getTemplatedComponentCodebehind(componentName, namespace));
-			}
+            //only create codebehind if namespace is given
+            if (namespaceNullable !== undefined && namespaceNullable.length > 0) {
+                let namespace: string = namespaceNullable;
+                await createFile(folderPath, `${componentName}.razor.cs`, getTemplatedComponentCodebehind(componentName, namespace));
+            }
 
-		})
-	);
+        })
+    );
 }
 
 async function createFile(directoryPath: string, newFileName: string, content: string) {
-	await fs.writeFile(path.join(directoryPath, newFileName), content, (err: any) => {
-		if (err) {
-			console.error(err);
-			return vscode.window.showErrorMessage("Failed to create " + newFileName);
-		}
+    await fs.writeFile(path.join(directoryPath, newFileName), content, (err: any) => {
+        if (err) {
+            console.error(err);
+            return vscode.window.showErrorMessage("Failed to create " + newFileName);
+        }
 
-		vscode.window.showInformationMessage("Created " + newFileName);
-	});
+        vscode.window.showInformationMessage("Created " + newFileName);
+    });
 }
